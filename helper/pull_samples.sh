f522f0ac45268f464ce31323f8f13d57cef0b754
Peyton Kuhlers <p.kuhl1711@gmail.com>
2023-10-06 01:16:16 -0400

    launch script checks pyairtable version

--- helper/pull_samples.sh
+++ helper/pull_samples.sh
@@ -8,6 +8,18 @@
 
 #SBATCH -J NF
 
+## Test pyairtable version
+module load python/3.8.8
+PYAIRTABLE_VERSION=$(python -c "import pyairtable; print(pyairtable.__version__)")
+if [[ $PYAIRTABLE_VERSION != '2.1.0.post1' ]]; then
+	echo "PLEASE UPDATE PYAIRTABLE"
+	echo "module load python/3.8.8"
+	echo "pip install --user pyairtable==2.1.0.post1"
+	exit 1
+fi
+module unload python
+
+## Run workflow
 module add nextflow
 source ~/.secrets/airtable
 
