913b16d107d7a389848fd1c02bc4c40140a4ec6a
Peyton Kuhlers <p.kuhl1711@gmail.com>
2026-01-20 11:53:32 -0500

    remove airtable stuff

--- helper/new_experiment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-module add nextflow
-source ~/.secrets/airtable
-
-## MODIFY THE EXPERIMENT ID FOR THE EXPERIMENT YOU WANT TO RUN
-nextflow run raab-lab/cut-n-run \
-	--new_experiment $1 \
-	-latest
